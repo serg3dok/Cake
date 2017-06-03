b5a177215cbbee7f6fec50c4dc119acfe82a8fe4
Sergey Okinchuk <serg3d@hotmail.com>
2017-06-03 14:16:52 -0700

    03. HighestProductOfThree solved

--- 03/src/HighestProductOfThree.java
+++ 03/src/HighestProductOfThree.java
@@ -1,42 +1,33 @@
 import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Created by sergeyo on 6/2/2017.
  */
 public class HighestProductOfThree {
     public static void main(String[] args) {
-        int[] testData = {1, 3, 5, 2, 4, 0, 7};
+        int[] testData = {1, 3, 5, 2, 4, 0, 7, -50, 25, -100};
         System.out.println(getHighestProductOfThree(testData));;
     }
 
     public static int getHighestProductOfThree(int[] input) {
 
-        List<Integer> positiveList = new LinkedList<Integer>();
-        List<Integer> negativeList = new LinkedList<Integer>();
+        LinkedList<Integer> positiveList = new LinkedList<Integer>();
+        LinkedList<Integer> negativeList = new LinkedList<Integer>();
         positiveList.add(0);
         negativeList.add(0);
 
         for (int current : input) {
             if (current < 0 ) {
-                if (current > negativeList.get(0) && negativeList.size() < 2) {
-                    int tmp = negativeList.get(0);
-                    negativeList.add(0, current);
-                    negativeList.add(0, tmp);
-                } else if (current < negativeList.get(0)) {
-                    negativeList.add(0, current);
-                }
+
+                pushToNegativeList(negativeList, current);
+
                 if (negativeList.size() > 2) {
                     negativeList.remove(2);
                 }
             } else  {
-                if (current < positiveList.get(0) && positiveList.size() < 3) {
-                    int tmp = positiveList.get(0);
-                    positiveList.add(0, current);
-                    positiveList.add(0, tmp);
-                } else if (current > positiveList.get(0)) {
-                    positiveList.add(0, current);
-                }
+
+                positiveList = pushToPositiveList(positiveList, current);
+
                 if (positiveList.size() > 3) {
                     positiveList.remove(3);
                 }
@@ -55,5 +46,29 @@ public class HighestProductOfThree {
 
 
         return Math.max(maxPositive * positiveProduct, maxPositive * negativeProduct);
+    }
+
+    public static LinkedList<Integer> pushToPositiveList(LinkedList<Integer> list, int num) {
+        int i = 0;
+        while (i < list.size() && list.get(i) > num) {
+            if (i > 2) {
+                return list;
+            }
+            i++;
+        }
+        list.add(i, num);
+        return list;
+    }
+
+    public static LinkedList<Integer> pushToNegativeList(LinkedList<Integer> list, int num) {
+        int i = 0;
+        while (i < list.size() && list.get(i) < num) {
+            if (i > 1) {
+                return list;
+            }
+            i++;
+        }
+        list.add(i, num);
+        return list;
     }
 }
