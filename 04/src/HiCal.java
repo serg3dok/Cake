6a804cf4509feea8dce0535b3c3c833e1dbf1132
Sergey Okinchuk <serg3d@hotmail.com>
2017-06-11 21:33:43 -0700

    04. HiCal solution. solved

--- 04/src/HiCal.java
+++ 04/src/HiCal.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -10,8 +12,23 @@ public class HiCal {
     protected static class Meeting {
         int startTime;
         int endTime;
+        //int sortedLength = 1;
+
+        static private Comparator<Meeting> comparatorByStartTime;
+        static {
+            comparatorByStartTime = new Comparator<Meeting>() {
+                @Override
+                public int compare(Meeting o1, Meeting o2) {
+                    return o1.startTime - o2.startTime;
+                }
+            };
+        }
         public Meeting(int startTime, int endTime) {
             this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        public void setEndTime(int endTime) {
             this.endTime = endTime;
         }
 
@@ -26,31 +43,68 @@ public class HiCal {
 
     public static void main(String[] args) {
 
-        Meeting[] meetings = new Meeting[3];
+        HiCal calendar = new HiCal();
+        Meeting[] meetings = new Meeting[4];
         meetings[0] = new Meeting(1, 2);
         meetings[1] = new Meeting(2, 3);
         meetings[2] = new Meeting(7, 9);
+        meetings[3] = new Meeting(6, 10);
 
-        Meeting[] mergedMeetings = merge(meetings);
-        printMeetings(mergedMeetings);
+        meetings = calendar.sortMeetings(meetings);
+        List<Meeting> mergedMeetings = calendar.merge(meetings);
 
-
+        calendar.printMeetings(mergedMeetings);
 
     }
 
-    public static Meeting[] merge(Meeting[] input) {
+    public List<Meeting> merge(Meeting[] input) {
         // merge meetings
         List<Meeting> mergedMeetings = new ArrayList<Meeting>();
+        mergedMeetings.add(input[0]);
+        int mergedIndex = 0;
+        for (Meeting m : input) {
+            if (m == input[0]) continue;
+
+            if (m.startTime <= mergedMeetings.get(mergedIndex).endTime &&
+                    m.endTime > mergedMeetings.get(mergedIndex).endTime) {
+                // case of merge
+                mergedMeetings.get(mergedIndex).setEndTime(m.getEndTime());
+            }
+            else if (m.startTime >= mergedMeetings.get(mergedIndex).startTime &&
+                       m.endTime <= mergedMeetings.get(mergedIndex).endTime ) {
+                    continue;
 
 
-        return input;
+            } else {
+                mergedMeetings.add(m);
+                mergedIndex++;
+            }
+
+        }
+
+        return mergedMeetings;
     }
 
-    public static void printMeetings(Meeting[] meetingsToPrint) {
+    public void printMeetings(List<Meeting> meetingsToPrint) {
         for (Meeting m : meetingsToPrint) {
+
             System.out.print(m.getStartTime() + " " + m.getEndTime());
             System.out.println();
         }
+
+    }
+
+
+    public Meeting[] sortMeetings(Meeting[] meetings) {
+
+        Arrays.sort(meetings, new Comparator<Meeting>() {
+            public int compare(Meeting m1, Meeting m2) {
+                return m1.startTime - m2.startTime;
+            }
+
+        } );
+
+        return meetings;
     }
 
 
